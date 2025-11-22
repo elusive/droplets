37f5a0d2a09a936b040b9f3d97f089c22f3e50c7
John Gilliland <john.gilliland@starz.com>
2025-11-21 21:45:40 -0800

    Simplify app component, add route for droplets feature main page, add global styles.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { DropletsComponent } from './features/droplets/pages/droplets-main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: DropletsComponent }];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './core/layout/header.component';
+import { Footer } from './core/layout/footer.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, Footer, Header],
+    template: ` <div class="wrapper">
+        <app-header></app-header>
+        <router-outlet />
+        <app-footer></app-footer>
+    </div>`,
+    styles: [
+        `
+            .wrapper {
+                display: grid;
+                grid-template-rows: auto 1fr auto; /* Header, main content, footer */
+                min-height: 100vh;
+            }
+        `,
+    ],
 })
 export class App {
-  protected readonly title = signal('droplets');
+    //protected readonly title = signal('droplets');
 }
