42ab3f4f2d3ed852a008e17790427e5a80ec8018
John Gilliland <john.gilliland@starz.com>
2025-11-23 22:04:28 -0800

    Fix path in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
-import { DropletsComponent } from './features/droplets/pages/droplets-main.component';
+import { DropletsComponent } from '@src/app/features/droplets/pages/droplets-main/droplets-main.component';
 
 export const routes: Routes = [{ path: '', component: DropletsComponent }];
